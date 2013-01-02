891ebea2525703319b7515dd1245a5fae3481be2
Satabdi Das <satabdidas@gmail.com>
2013-01-02 23:03:54 +0530

    1. adding code in test-geoip-addr-lookup.c to give output in json format 2. Make necessary changes in Makefile 3. Adding the json format of the output in README file

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 CC= gcc
-CFLAGS= -c -Wall
-LDFLAGS= -lGeoIP
+CFLAGS= -g -c -Wall -I/usr/include/ -I/usr/include/json-glib-1.0 -I/home/satabdi/src/myopt/gnome/include/glib-2.0 -I/home/satabdi/src/myopt/gnome/lib/glib-2.0/include
+LDFLAGS= -L/home/satabdi/src/myopt/gnome/lib -lGeoIP -ljson-glib-1.0 -lgobject-2.0 -lglib-2.0
 SOURCES= test-geoip-addr-lookup.c
 OBJECTS= $(SOURCES:.c=.o)
 EXECUTABLE= test-geoip-addr-lookup
--- test-geoip-addr-lookup.c
+++ test-geoip-addr-lookup.c
@@ -1,25 +1,79 @@
-#include <GeoIP.h>
-#include <GeoIPCity.h>
+#include "GeoIP.h"
+#include "GeoIPCity.h"
+#include "glib-object.h"
+#include "json-glib/json-glib.h"
+
+static void
+add_result_attr_to_json_tree (const char* ipaddress, GeoIPRecord *gir) {
+
+  gdouble latitude;
+  gdouble longitude;
+  gchar *jsondata;
+  gsize length;
+  JsonBuilder *builder;
+  JsonNode *node;
+  JsonGenerator *generator;
+
+  builder = json_builder_new ();
+
+  json_builder_begin_object (builder);
+
+  json_builder_set_member_name (builder, "results");
+  json_builder_begin_array (builder);
+  json_builder_begin_object (builder);
+
+  json_builder_set_member_name (builder, "ipaddress");
+  json_builder_add_string_value (builder, ipaddress);
+
+  latitude = gir->latitude;
+  longitude = gir->longitude;
+  json_builder_set_member_name (builder, "location");
+  json_builder_begin_object (builder);
+  json_builder_set_member_name (builder, "latitude");
+  json_builder_add_double_value (builder, latitude);
+  json_builder_set_member_name (builder, "longitude");
+  json_builder_add_double_value (builder, longitude);
+  json_builder_end_object (builder); /* end location */
+
+  json_builder_end_object (builder);
+  json_builder_end_array (builder); /*end results */
+
+  json_builder_end_object (builder);
+
+  node = json_builder_get_root (builder);
+  g_object_unref (builder);
+
+  generator = json_generator_new ();
+  json_generator_set_root (generator, node);
+  jsondata = json_generator_to_data (generator, &length);
+  g_print ("%*s\n", (int)length, jsondata);
+
+  g_free(jsondata);
+  json_node_free(node);
+  g_object_unref(generator);
+}
 
 int 
 main(int argc, char **argv) {
   GeoIP *gi;
   GeoIPRecord *gir;
-  const char * ipaddr;
-  float latitude, longitude;
+  const char * ipaddress;
 
-  ipaddr = argv[1];
+  g_type_init();
+  ipaddress = argv[1];
   gi = GeoIP_open("./GeoLiteCity.dat", GEOIP_INDEX_CACHE);
   if(gi == NULL) {
     printf("ERROR opening database\n");
   }
-  gir = GeoIP_record_by_name(gi, ipaddr);
+  gir = GeoIP_record_by_addr(gi, ipaddress);
 
   if(gir != NULL) {
-    latitude = gir->latitude;
-    longitude = gir->longitude;
+    /* Add the result attributes to the Json tree
+       at present only add the latitude and longitude
+       of the place*/
     printf("This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com\n");
-    printf("Latitude %f Longitude %f\n", latitude, longitude);
+    add_result_attr_to_json_tree(ipaddress, gir);
+
     GeoIPRecord_delete(gir);
   }
   GeoIP_delete(gi);
