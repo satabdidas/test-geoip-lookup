cba3fa57cf7d358b1f58736f37595890bc52c020
Satabdi Das <satabdidas@gmail.com>
2013-01-04 17:49:33 +0530

    some changes in the coding style to make it look prettier

--- test-geoip-addr-lookup.c
+++ test-geoip-addr-lookup.c
@@ -25,14 +25,12 @@ add_result_attr_to_json_tree (const char* ipaddress, GeoIPRecord *gir) {
   json_builder_set_member_name (builder, "ipaddress");
   json_builder_add_string_value (builder, ipaddress);
 
-  latitude = gir->latitude;
-  longitude = gir->longitude;
   json_builder_set_member_name (builder, "location");
   json_builder_begin_object (builder);
   json_builder_set_member_name (builder, "latitude");
-  json_builder_add_double_value (builder, latitude);
+  json_builder_add_double_value (builder, gir->latitude);
   json_builder_set_member_name (builder, "longitude");
-  json_builder_add_double_value (builder, longitude);
+  json_builder_add_double_value (builder, gir->longitude);
   json_builder_end_object (builder); /* end location */
 
   json_builder_end_object (builder);
@@ -48,34 +46,34 @@ add_result_attr_to_json_tree (const char* ipaddress, GeoIPRecord *gir) {
   jsondata = json_generator_to_data (generator, &length);
   g_print ("%*s\n", (int)length, jsondata);
 
-  g_free(jsondata);
-  json_node_free(node);
-  g_object_unref(generator);
+  g_free (jsondata);
+  json_node_free (node);
+  g_object_unref (generator);
 }
 
 int 
-main(int argc, char **argv) {
+main (int argc, char **argv) {
   GeoIP *gi;
   GeoIPRecord *gir;
   const char * ipaddress;
 
-  g_type_init();
+  g_type_init ();
   ipaddress = argv[1];
-  gi = GeoIP_open("./GeoLiteCity.dat", GEOIP_INDEX_CACHE);
-  if(gi == NULL) {
-    printf("ERROR opening database\n");
+  gi = GeoIP_open ("./GeoLiteCity.dat", GEOIP_INDEX_CACHE);
+  if (gi == NULL) {
+    printf ("ERROR opening database\n");
   }
-  gir = GeoIP_record_by_addr(gi, ipaddress);
+  gir = GeoIP_record_by_addr (gi, ipaddress);
 
-  if(gir != NULL) {
+  if (gir != NULL) {
     /* Add the result attributes to the Json tree
        at present only add the latitude and longitude
        of the place*/
-    printf("This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com\n");
-    add_result_attr_to_json_tree(ipaddress, gir);
+    printf ("This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com\n");
+    add_result_attr_to_json_tree (ipaddress, gir);
 
-    GeoIPRecord_delete(gir);
+    GeoIPRecord_delete (gir);
   }
-  GeoIP_delete(gi);
+  GeoIP_delete (gi);
   return 0;
 }
